5c3db7adb70680650d7ee5fcad641302d7b85099
Jacob <weaver.jacob.t@gmail.com>
2023-09-07 16:32:23 -0500

    scoped

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+
+
+function saturdayFun (dftAct = 'roller-skate' ) {
+    return `This Saturday, I want to ${dftAct}!`;
+    
+}
+
+
+function mondayWork (dftWrk = 'go to the office') {
+    return `This Monday, I will ${dftWrk}.`;
+
+}
+
+
+function wrapAdjective (flr1 = '*') {
+    
+    return function (dftAdj = 'special') {
+        
+    return `You are ${flr1}${dftAdj}${flr1}!`
+
+    
+}
+  //dfrWrp = '||'
+
+}
